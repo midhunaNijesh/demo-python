2b69156763190c689e4294457c7e99f561c4c0d7
midhunaNiejsh <midhunanijesh11@gmail.com>
2022-11-30 09:33:18 +0000

    create python program

--- python.py
+++ python.py
@@ -1,9 +1,26 @@
-# Store input numbers
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+# Python program showing how to
+# multiple input using split
 
-# Add two numbers
-sum = float(num1) + float(num2)
+# taking two inputs at a time
+x, y = input("Enter two values: ").split()
+print("Number of boys: ", x)
+print("Number of girls: ", y)
+print()
 
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+# taking three inputs at a time
+x, y, z = input("Enter three values: ").split()
+print("Total number of students: ", x)
+print("Number of boys is : ", y)
+print("Number of girls is : ", z)
+print()
+
+# taking two inputs at a time
+a, b = input("Enter two values: ").split()
+print("First number is {} and second number is {}".format(a, b))
+print()
+
+# taking multiple inputs at a time
+# and type casting using list() function
+x = list(map(int, input("Enter multiple values: ").split()))
+print("List of students: ", x)
+
